9ca37bf2633feb1b696487b67a188bb3492b7696
tama3 <tama3@acee48c3-7b26-0410-bdac-b3d0e5314bbc>
2008-12-22 05:32:14 +0000

    update test code
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/stigmata/trunk@363 acee48c3-7b26-0410-bdac-b3d0e5314bbc

--- src/test/java/jp/sourceforge/stigmata/birthmarks/kgram/KGramTest.java
+++ src/test/java/jp/sourceforge/stigmata/birthmarks/kgram/KGramTest.java
@@ -4,8 +4,7 @@ package jp.sourceforge.stigmata.birthmarks.kgram;
  * $Id$
  */
 
-import static org.junit.Assert.assertEquals;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,50 +26,50 @@ public class KGramTest{
     @Test
     public void checkPlainKGram(){
         KGram<String>[] kgrams = KGramBuilder.getInstance().buildKGram(plainValues, 4);
-        assertEquals(4, kgrams.length);
+        Assert.assertEquals(4, kgrams.length);
 
-        assertEquals(4, kgrams[0].getKValue());
-        assertEquals(4, kgrams[1].getKValue());
-        assertEquals(4, kgrams[2].getKValue());
-        assertEquals(4, kgrams[3].getKValue());
+        Assert.assertEquals(4, kgrams[0].getKValue());
+        Assert.assertEquals(4, kgrams[1].getKValue());
+        Assert.assertEquals(4, kgrams[2].getKValue());
+        Assert.assertEquals(4, kgrams[3].getKValue());
 
-        assertEquals(new String[] { "a", "b", "c", "d", }, kgrams[0].toArray());
-        assertEquals(new String[] { "b", "c", "d", "e", }, kgrams[1].toArray());
-        assertEquals(new String[] { "c", "d", "e", "f", }, kgrams[2].toArray());
-        assertEquals(new String[] { "d", "e", "f", "g", }, kgrams[3].toArray());
+        Assert.assertArrayEquals(new String[] { "a", "b", "c", "d", }, kgrams[0].toArray());
+        Assert.assertArrayEquals(new String[] { "b", "c", "d", "e", }, kgrams[1].toArray());
+        Assert.assertArrayEquals(new String[] { "c", "d", "e", "f", }, kgrams[2].toArray());
+        Assert.assertArrayEquals(new String[] { "d", "e", "f", "g", }, kgrams[3].toArray());
     }
 
     @Test
     public void checkPlainKGram2(){
         KGram<String>[] kgrams = KGramBuilder.getInstance().buildKGram(plainValues, 3);
-        assertEquals(5, kgrams.length);
+        Assert.assertEquals(5, kgrams.length);
 
-        assertEquals(3, kgrams[0].getKValue());
-        assertEquals(3, kgrams[1].getKValue());
-        assertEquals(3, kgrams[2].getKValue());
-        assertEquals(3, kgrams[3].getKValue());
-        assertEquals(3, kgrams[4].getKValue());
+        Assert.assertEquals(3, kgrams[0].getKValue());
+        Assert.assertEquals(3, kgrams[1].getKValue());
+        Assert.assertEquals(3, kgrams[2].getKValue());
+        Assert.assertEquals(3, kgrams[3].getKValue());
+        Assert.assertEquals(3, kgrams[4].getKValue());
 
-        assertEquals(new String[] { "a", "b", "c", }, kgrams[0].toArray());
-        assertEquals(new String[] { "b", "c", "d", }, kgrams[1].toArray());
-        assertEquals(new String[] { "c", "d", "e", }, kgrams[2].toArray());
-        assertEquals(new String[] { "d", "e", "f", }, kgrams[3].toArray());
-        assertEquals(new String[] { "e", "f", "g", }, kgrams[4].toArray());
+        Assert.assertArrayEquals(new String[] { "a", "b", "c", }, kgrams[0].toArray());
+        Assert.assertArrayEquals(new String[] { "b", "c", "d", }, kgrams[1].toArray());
+        Assert.assertArrayEquals(new String[] { "c", "d", "e", }, kgrams[2].toArray());
+        Assert.assertArrayEquals(new String[] { "d", "e", "f", }, kgrams[3].toArray());
+        Assert.assertArrayEquals(new String[] { "e", "f", "g", }, kgrams[4].toArray());
     }
 
     @Test
     public void checkComplexKGram(){
         KGram<String>[] kgrams = KGramBuilder.getInstance().buildKGram(complexValues, 3);
 
-        assertEquals(7, kgrams.length);
+        Assert.assertEquals(7, kgrams.length);
 
-        assertEquals(new String[] { "a", "b", "r", }, kgrams[0].toArray());
-        assertEquals(new String[] { "b", "r", "a", }, kgrams[1].toArray());
-        assertEquals(new String[] { "r", "a", "c", }, kgrams[2].toArray());
-        assertEquals(new String[] { "a", "c", "a", }, kgrams[3].toArray());
-        assertEquals(new String[] { "c", "a", "d", }, kgrams[4].toArray());
-        assertEquals(new String[] { "a", "d", "a", }, kgrams[5].toArray());
-        assertEquals(new String[] { "d", "a", "b", }, kgrams[6].toArray());
+        Assert.assertArrayEquals(new String[] { "a", "b", "r", }, kgrams[0].toArray());
+        Assert.assertArrayEquals(new String[] { "b", "r", "a", }, kgrams[1].toArray());
+        Assert.assertArrayEquals(new String[] { "r", "a", "c", }, kgrams[2].toArray());
+        Assert.assertArrayEquals(new String[] { "a", "c", "a", }, kgrams[3].toArray());
+        Assert.assertArrayEquals(new String[] { "c", "a", "d", }, kgrams[4].toArray());
+        Assert.assertArrayEquals(new String[] { "a", "d", "a", }, kgrams[5].toArray());
+        Assert.assertArrayEquals(new String[] { "d", "a", "b", }, kgrams[6].toArray());
         // following kgram is appeared in above.
         // assertEquals(new String[] { "a", "b", "r", }, kgrams[0].toArray());
         // assertEquals(new String[] { "b", "r", "a", }, kgrams[1].toArray());
